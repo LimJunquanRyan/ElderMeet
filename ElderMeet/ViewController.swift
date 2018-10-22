0e156ffaec7799e2c580642e265e394b42fe77d6
Ryanljq <lim_junquan_ryan@s2017.sst.edu.sg>
2018-10-22 14:15:59 +0800

    Initial Commit

---
+++ ElderMeet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElderMeet
+//
+//  Created by Lim JunQuan Ryan on 22/10/18.
+//  Copyright © 2018 Ryan Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
